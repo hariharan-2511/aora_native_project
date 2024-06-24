e4942303b3c621272e583b59da7ca542fbf1bd15
Hari <hariroriri25Gmail.com>
2024-06-24 16:58:21 +0530

    _layout(tab) commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import { Tabs, Redirect } from 'expo-router';
+
+import { icons } from '../../constants';
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image 
+        source={icon}
+        resizedMode="contain"
+      />
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs>
+        <Tabs.Screen 
+          name="home"
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => {
+              <TabIcon 
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            }
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
 
